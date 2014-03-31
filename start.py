b89dddc639f705738a8da4263e0a358f30786338
leahsteinberg <leahsteinberg1@gmail.com>
2014-03-31 15:57:15 -0400

    made sure mmafia file was added

--- start.py
+++ start.py
@@ -13,12 +13,12 @@ beginning_players = []
 
 
 @app.route("/", methods=['GET', 'POST'])
-def mafia_game():
-  
+def mafia_game(): 
   #if game_state == 'joining':
     #if request.values.get('From', None) not in players_counts:
-  message = mafia.player_join(request)
-  resp = twilio.twiml.Response().message(message)
+  join_msg = mafia.player_join(request)
+  resp = twilio.twiml.Response()
+  resp.message(join_msg)
   return str(resp)
 
     # elif player_counts[request.values.get('From', None)] > 0:
