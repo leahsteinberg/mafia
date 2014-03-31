cc55d966fd69aa7c5a4cc97f9368201a8924686b
leahsteinberg <leahsteinberg1@gmail.com>
2014-03-31 15:42:12 -0400

    setting up mafia logic

---
+++ mafia.py
@@ -0,0 +1,73 @@
+
+from twilio.rest import TwilioRestClient
+townsfolk = []
+mafiosos = []
+innocents = []
+groups = {"townsfolk": townsfolk, "mafiosos": mafiosos, "innocents": innocents}
+account = "C8713d29f391c6ccdc9b9942d98a407df"
+token = "36c9a371567932584ad625db360f3be"
+client = TwilioRestClient(account, token)
+
+
+
+def send_group(group_str, announcement):
+  for person in groups[group_str]:
+    message = client.sms.messages.create(to=person.number, from_="+17472335925",
+	                                     body=announcement)
+
+
+def player_join(message):
+  new_number = message.values.get('From', None)
+  response = twilio.twiml.Response()
+  response.message("Welcome to this little town. Respond with your name.")
+  return new_number
+
+def player_init(message):
+  text = message.values.get('Body', None)
+  for word in text.lower().split():
+    if word not in ['hi', 'i', 'am', "i'm", 'im']:
+      name = word
+      break
+  player = Player(name, message.values.get('From', None))
+  townsfolk.append(player)
+  return player
+
+class Player():
+  def __init__(name, number):
+    self.mafia = False
+    self.alive = True
+    self.number = number
+    self.name = name
+    self.cookie = None
+
+
+    #initialization:
+
+
+# round: night
+#   EVERYONE:
+#   say it's night time to everyone:
+
+#   MAFIA:
+#   introduce the mafiosos to their secret room:
+#   mafiosos have a secret "room" to discuss who they wanna kill
+#   once they reach consensus, tell them who they have decided.
+#   they can send things to discuss, and when they want to kill someone
+
+#   TOWNSPEOPLE
+#   // during night time,, need somethign so all townspeople 
+#    are writing something to eachother..?? what would be funny but also keep them
+#    // quiet?
+#    // need fakey night time thing
+
+#   dawn:
+
+#     send out who got killed
+
+# Day:
+#   send good morning to everyone
+#   say who died the previous night
+#   send the person who died a skull thing
+#   discussion
+#   then everyone votes
+#   if half the players vote to kill a mafioso, that person is eliminated.
--- start.py
+++ start.py
@@ -2,25 +2,48 @@ from twilio.rest import TwilioRestClient
 from flask import Flask, request, redirect
 import twilio.twiml
 from emoji import e
+import mafia
 
 app = Flask(__name__)
-callers = {"+18185218419": "leah"}
+
+player_counts = {}
+game_state = ['joining', 'beginning', 'night', 'dawn', 'day']
+game_state = 'joining'
+beginning_players = []
 
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-  from_number = request.values.get('From', None)
-  they_sent = request.values.get('Body', None)
-  message = ''
-  if they_sent in e.keys():
-    message += e[they_sent] + "  "
-  if from_number in callers:
-    message += callers[from_number]
-  else:
-    message += u" \U0001F31F"
-  resp = twilio.twiml.Response()
-  resp.message(message)
-  return str(resp)
+def mafia_game():
+  if game_state == 'joining':
+    if request.values.get('From', None) not in players_counts:
+      player_counts[request.values.get('From', None)] = 0
+      new_number = mafia.player_join(request)
+    elif player_counts[request.values.get('From', None)] > 0:
+      player_init(request)
+      send_group('townsfolk', 'can_begin')
+  #send message saying that if any of them respond with
+	  # a certain phrase, the game will begin, but until then more 
+	  #people may join.
+    if "begin" in request.values.get('Body', None):
+      game_state = 'begin'
+  return None
+    
+
+
+
+
+  # from_number = request.values.get('From', None)
+  # they_sent = request.values.get('Body', None)
+  # message = ''
+  # if they_sent in e.keys():
+  #   message += e[they_sent] + "  "
+  # if from_number in callers:
+  #   message += callers[from_number]
+  # else:
+  #   message += u" \U0001F31F"
+  # resp = twilio.twiml.Response()
+  # resp.message(message)
+  # return str(resp)
 
 if __name__ == '__main__':
   app.run(debug=True)
